fd35a1f86184ccfb5647f857b65cc6f1621d5c10
Paulo Cunha <cunha.paulo.doc@gmail.com>
2026-09-22 08:23:01 -0300

    Update

--- .cmd/update.ps1
+++ .cmd/update.ps1
@@ -1,3 +1,5 @@
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
 #-----------------------------------------------
 #--- Clear the screen 
 #-----------------------------------------------
